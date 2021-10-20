ee490b3d5f31d6dba2b72e77808060d931e3f1d4
hongjs <hjs21847@naver.com>
2021-10-20 22:01:05 +0900

    문제 풀이

---
+++ 1. 부족한 금액.js
@@ -0,0 +1,15 @@
+function solution(price, money, count) {
+    let answer = -1;
+
+    for (let i = 1; i <= count; i++) {
+        money -= price * i;
+    }
+
+    if (money > 0) {
+        answer = 0;
+    } else {
+        answer = money * -1;    
+    }
+
+    return answer;
+}
